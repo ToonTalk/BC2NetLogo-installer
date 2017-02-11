d3abfdc57794218a8f2837fa9fcd88e848eeb5e2
Ken Kahn <toontalk@gmail.com>
2017-02-11 19:17:09 +0800

    Changed default server

--- BC2NLInstaller/src/uk/ac/ox/it/modelling4all/bc2nlinstaller/BC2NetLogoInstaller.java
+++ BC2NLInstaller/src/uk/ac/ox/it/modelling4all/bc2nlinstaller/BC2NetLogoInstaller.java
@@ -66,8 +66,7 @@ public class BC2NetLogoInstaller {
     		}
     	}
     	boolean previouslyInstalled = true;
-    	// using 800 for NetLogo 6.0 while NetLogo Web uses 791 which is still 5.3.1
-    	String defaultServer = "https://800-dot-m4a-gae-hrd.appspot.com"; // "https://m4a-gae.appspot.com";
+    	String defaultServer = "https://m4a-gae.appspot.com";
     	String server = Settings.getPreference("server", defaultServer);
     	if (!previouslyInstalled) {
     		try {
